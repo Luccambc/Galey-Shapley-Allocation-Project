9873f90dfb9fd3b3700cac8b3ad694c6a263a1f2
Leandro Kornelius <leobelko@gmail.com>
2024-06-21 02:02:00 -0300

    ✨ feat: file read and some minor fixes to the add functions for edge cases

--- solution.py
+++ solution.py
@@ -4,12 +4,18 @@ students = {"cod" : [[student project preferences in order], student qualificati
 projects = {"cod" : [number of available slots, required qualification]}
 '''
 
+projects = {}
+availableStudents = {}
+notAlocatedStudents = {}
+
 def addProject(projectLine):
-    removingParenthesis = projectLine[1:len(projectLine) - 1]
+    removingParenthesis = projectLine[1:-1]
     # print(removingParenthesis)
     separatingInformation = removingParenthesis.split(", ")
     # print(separatingInformation)
-    projects[separatingInformation[0]] = list(map(int, separatingInformation[1:3]))
+    projectCode = separatingInformation[0]
+    slots, qualification = map(int, separatingInformation[1:3])
+    projects[projectCode] = [slots, qualification]
     # print(projects)
 
 def addStudent(studentLine):
@@ -19,18 +25,30 @@ def addStudent(studentLine):
     provCode2 = provCode1.replace("(", "")
     studentCode = provCode2.replace(")", "")
     # print(studentCode)
-    separatingInformation = projectInformation.split(") (")
+    if studentCode == 'A177':
+        separatingInformation = projectInformation.split(")(")
+    else:
+        separatingInformation = projectInformation.split(") (")
     # print(separatingInformation)
     studentPreferences = (separatingInformation[0].replace("(", "")).split(", ")
     # print(studentPreferences)
     studentQualification = int(separatingInformation[1].replace(")", ""))
     # print(studentQualification)
     availableStudents[studentCode] = [studentPreferences, studentQualification]
-    print(availableStudents)
+    # print(availableStudents)
 
-projects = {}
-availableStudents = {}
-notAlocatedStudents = {}
+
+def processFile(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line or line.startswith("//"):
+                continue
+            if "A" in line:
+                print(availableStudents)
+                addStudent(line)
+            elif "P" in line:
+                addProject(line)
 
 """
 Testing the addDataStructure for some cases:
@@ -38,4 +56,11 @@ addProject("(P1, 3, 5)")
 addStudent("(A1):(P1, P30, P50) (5)")
 addStudent("(A198):(P2, P50) (3)")
 """
+
+processFile('dataEntry.txt')
+
+print(projects)
+print(availableStudents)
+
+
 
